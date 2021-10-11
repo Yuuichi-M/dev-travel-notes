2be555b72a3a4d6ebf7adf8c059594db95f8b4b8
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2021-10-11 22:05:59 +0900

    投稿画面修正

--- src/resources/views/articles/create.blade.php
+++ src/resources/views/articles/create.blade.php
@@ -13,9 +13,9 @@
 
             <div class="card mt-4 rounded">
 
-                <h4 class="card-header font-weight-bold text-center pb-3 pt-3">
-                    <i class="fas fa-paper-plane deep-orange-text" style="font-size: 24px"></i>
-                    <span class="deep-orange-text">POST</span>
+                <h4 class="card-header font-weight-bold deep-orange lighten-1 text-center pb-3 pt-3">
+                    <i class="fas fa-paper-plane text-white" style="font-size: 24px"></i>
+                    <span class="text-white">POST</span>
                 </h4>
 
                 <div class="card-body text-center">
@@ -26,7 +26,7 @@
 
                             @include('articles.form')
 
-                            <button class="btn btn-block btn-deep-orange rounded-pill mt-4 text-white" title="投稿" type="submit">
+                            <button class="btn btn-block deep-orange lighten-1 rounded-pill mt-4 text-white" title="投稿" type="submit">
                                 <i class="far fa-paper-plane text-white"></i>
                                 POST
                             </button>
