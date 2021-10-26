b13ad9956e7796b482c7d5611f171c64bd71a870
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2021-10-26 18:32:15 +0900

    タグ・いいね機能表示修正

--- src/resources/views/articles/tag.blade.php
+++ src/resources/views/articles/tag.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group mt-2 mb-2">
+<div class="form-group pt-0 pb-3 pl-3">
     <article-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'>
     </article-tags-input>
 </div>
