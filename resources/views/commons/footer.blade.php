b021d5827520806862d87971c28317c154b56263
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2022-03-05 01:24:18 +0900

    fix: ヘッダー,フッター,ウェルカム画面のUI修正

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="footer mt-auto py-4 border-top shadow-none">
+<footer class="footer mt-auto pt-4 border-top shadow-none">
     <div class="container">
         <span class="d-flex justify-content-center flex-wrap deep-orange-text small pt-1">
-            <address class="small">
+            <address>
                 Copyright &copy; 2022 Yuuichi Mizuta All Rights Reserved.
             </address>
         </span>
